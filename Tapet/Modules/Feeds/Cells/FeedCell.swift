87679f883e5e47303bdcabf93d00eafb6c576fcb
Lizan P <lizanp@def.local>
2019-09-11 14:03:41 +0545

    latest

--- Tapet/Modules/Feeds/Cells/FeedCell.swift
+++ Tapet/Modules/Feeds/Cells/FeedCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SDWebImage
 
-class FeedCell: UITableViewCell {
+class FeedCell: UITableViewCell, RealmPersistenceType {
 
     @IBOutlet weak var userProfile: UIImageView!
     @IBOutlet weak var userName: UILabel!
@@ -21,6 +21,11 @@ class FeedCell: UITableViewCell {
     
     var model: PictureModel? {
         didSet {
+            if UIViewController().getAllIds().contains(model?.id ?? "") {
+                self.likeBtn.setImage(UIImage.init(named: "heart"), for: .normal)
+            } else {
+                self.likeBtn.setImage(UIImage.init(named: "fav"), for: .normal)
+            }
             self.userProfile.sd_setImage(with: URL.init(string: model?.user?.profile_image?.small ?? ""), placeholderImage: UIImage.init(named: "3336938-64"), options: .refreshCached) { (_, _, _, _) in
             }
             self.userProfile.round()
@@ -34,6 +39,25 @@ class FeedCell: UITableViewCell {
                 }
             }
             likes.text = "\(model?.likes ?? 0)"
+            
+            likeBtn.addTarget(self, action: #selector(like), for: .touchUpInside)
+        }
+    }
+    
+    @objc func like() {
+        if UIViewController().getAllIds().contains(model?.id ?? "") {
+            let models : [PictureRealmModel] = self.fetch()
+            let thisModel = models.filter{$0.id == model?.id ?? ""}
+            self.delete(models: thisModel)
+            likes.text = "\((self.model?.likes ?? 0) - 1)"
+            self.likeBtn.setImage(UIImage.init(named: "fav"), for: .normal)
+            return
+        }
+        self.model?.imageData = self.userImage.image?.sd_imageData()
+        if let model = model?.toRealm() {
+            self.save(models: [model])
+             likes.text = "\((self.model?.likes ?? 0) + 1)"
+            likeBtn.setImage(UIImage.init(named: "heart"), for: .normal)
         }
     }
 }
--- Tapet/Modules/Feeds/FeedsDetailsViewController.swift
+++ Tapet/Modules/Feeds/FeedsDetailsViewController.swift
@@ -16,23 +16,31 @@ class FeedsDetailsViewController: UIViewController {
     @IBOutlet weak var desc: UILabel!
     @IBOutlet weak var info: UILabel!
     @IBOutlet weak var feedImage: UIImageView!
+    @IBOutlet weak var favBtn: UIButton!
     
     var image: UIImage?
     var model: PictureModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+         admobDelegate.showAd()
         infoVIew.layer.cornerRadius = 5
         feedImage.hero.id = model?.id
         self.hero.isEnabled = true
         feedImage.sd_setImage(with: URL.init(string: model?.urls?.full ?? "") , placeholderImage: image, options: .refreshCached) { (_, _, _, _) in
             
         }
+        
+        if UIViewController().getAllIds().contains(model?.id ?? "") {
+            self.favBtn.setImage(UIImage.init(named: "heart"), for: .normal)
+        } else {
+            self.favBtn.setImage(UIImage.init(named: "fav"), for: .normal)
+        }
         view.isUserInteractionEnabled = true
         let swipe = UISwipeGestureRecognizer.init(target: self, action: #selector(cancel))
         swipe.direction = .down
         self.view.addGestureRecognizer(swipe)
-        self.info.text = model?.descriptions ?? model?.alt_description
+        self.info.text = "#Unsplash \n" + (model?.descriptions ?? model?.alt_description ?? "")
         self.desc.text = model?.alt_description
 //        getImages()
         // Do any additional setup after loading the view.
@@ -55,16 +63,18 @@ class FeedsDetailsViewController: UIViewController {
     }
     
     func savePhoto() {
+        self.startAnimating(type: .ballGridPulse)
         UIImageWriteToSavedPhotosAlbum(self.feedImage.image ?? UIImage(), self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
     
     @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        self.stopAnimating()
         if let error = error {
             let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default))
             present(ac, animated: true)
         } else {
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "SavedViewController") as! SavedViewController
+            let vc = UIStoryboard.init(name: "ImageDetails", bundle: nil).instantiateViewController(withIdentifier: "SavedViewController") as! SavedViewController
             vc.didDismiss = {
                 currentVc = self
                 if (admobDelegate.interstitialView.isReady == true){
@@ -77,7 +87,19 @@ class FeedsDetailsViewController: UIViewController {
     }
 
     @IBAction func favAction(_ sender: Any) {
-        
+        if UIViewController().getAllIds().contains(model?.id ?? "") {
+            let models : [PictureRealmModel] = self.fetch()
+            let thisModel = models.filter{$0.id == model?.id ?? ""}
+            self.delete(models: thisModel)
+            self.favBtn.setImage(UIImage.init(named: "fav"), for: .normal)
+            return
+        }
+        self.model?.imageData = self.feedImage.image?.sd_imageData()
+        if let model = model?.toRealm() {
+            self.save(models: [model])
+            
+            favBtn.setImage(UIImage.init(named: "heart"), for: .normal)
+        }
     }
     
     @IBAction func downloadAction(_ sender: Any) {
@@ -85,7 +107,10 @@ class FeedsDetailsViewController: UIViewController {
     }
     
     @IBAction func sendAction(_ sender: Any) {
-        
+        let imageShare = [ self.feedImage.image! ]
+        let activityViewController = UIActivityViewController(activityItems: imageShare , applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
     }
     
 }
